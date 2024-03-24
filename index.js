22ef8d3479b5c910009708023aaa4df30c413e31
jeff muriuki <jeff.muriuki@studet.moringaschool.com>
2024-03-24 20:56:24 +0300

    implement functions

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+
+function saturdayFun(activity ="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday I will ${activity}.`
+}
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective(flair = '*') {
+    return function (adjective = 'special') {
+        console.log(`You are ${flair}${adjective}${flair}!`);
+        
+    }
+}
+wrapAdjective()('a hard worker');
+wrapAdjective('||')('a dedicated programmer');
